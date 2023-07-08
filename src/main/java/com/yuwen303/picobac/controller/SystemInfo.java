71cbdcfed7efb76e978965bd8da5bdf2e991e3f6
Administrator <2110146041@qq.com>
2023-07-09 00:23:39 +0800

    SystemInfoUtil.java

--- src/main/java/com/yuwen303/picobac/controller/SystemInfo.java
+++ src/main/java/com/yuwen303/picobac/controller/SystemInfo.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/system", produces = MediaType.APPLICATION_JSON_VALUE)
 public class SystemInfo {
+    //
 
 
 }
---
+++ src/main/java/com/yuwen303/picobac/utils/SystemInfoUtil.java
@@ -0,0 +1,4 @@
+package com.yuwen303.picobac.utils;
+
+public class SystemInfoUtil {
+}
