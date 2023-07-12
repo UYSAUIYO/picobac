abc410c89a7de7841fb3dfaf6c0da7215fe2afbc
Administrator <2110146041@qq.com>
2023-07-12 12:04:09 +0800

    修正功能

--- src/main/java/com/yuwen303/picobac/controller/SystemInfoController.java
+++ src/main/java/com/yuwen303/picobac/controller/SystemInfoController.java
@@ -1,6 +1,7 @@
 package com.yuwen303.picobac.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.UnknownHostException;
 
 import static com.yuwen303.picobac.utils.SystemInfoUtil.getInfo;
+import static com.yuwen303.picobac.utils.SystemInfoUtil.reGetInfo;
 
 /**
  * @author YuWen
  */
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/system")
 public class SystemInfoController {
     @GetMapping("/info")
     public JSONObject systemInfoUtil() throws UnknownHostException {
         return getInfo();
+    }
+    @GetMapping("/regetinfo")
+    public JSONObject reInfo(){
+        return reGetInfo();
     }
 }
