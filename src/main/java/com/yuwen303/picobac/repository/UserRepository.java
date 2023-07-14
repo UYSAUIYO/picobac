c2ba21251a4b5e540479eacfb701ff9dbb3dd687
Administrator <2110146041@qq.com>
2023-07-14 21:07:33 +0800

    登录功能YuWen

--- src/main/java/com/yuwen303/picobac/repository/UserRepository.java
+++ src/main/java/com/yuwen303/picobac/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.yuwen303.picobac.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author YuWen
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
