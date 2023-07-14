4c8dd8ff3a407fade81b061059741a38b003f107
Administrator <2110146041@qq.com>
2023-07-14 21:13:57 +0800

    登录功能YuWen

--- src/main/java/com/yuwen303/picobac/controller/AuthController.java
+++ src/main/java/com/yuwen303/picobac/controller/AuthController.java
@@ -8,12 +8,14 @@ import com.yuwen303.picobac.utils.PassWordEncoderUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author YuWen
  */
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
     private final JwtUtil jwtUtil;
 
